71eade263c8bf7085a0b3531b96e522d328f58ee
Bryan-van-Riessen-theBvR <75975096+Bryan-van-Riessen-theBvR@users.noreply.github.com>
2020-12-15 11:17:55 +0100

    Update besteld.php
    Added some text in the file along with the header and footer

--- pages/besteld.php
+++ pages/besteld.php
@@ -1,1 +1,3 @@
-
+<?php include('../include/header.inc.php');?>
+ print "<h1> Uw bestelling is geplaatst. Your order has been placed. Check alstublieft uw Email voor een bevestigings mail. Please check your Email for a confirmation email.</h1>"
+<?php  include('../include/footer.inc.php') ?>
