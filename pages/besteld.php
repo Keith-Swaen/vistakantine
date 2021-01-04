c3ab3e1218086f9dd983abcb7133be0b0616f5ce
Bryan-van-Riessen-theBvR <75975096+Bryan-van-Riessen-theBvR@users.noreply.github.com>
2021-01-04 11:16:13 +0100

    Update besteld.php

--- pages/besteld.php
+++ pages/besteld.php
@@ -1,3 +1,4 @@
 <?php include('../include/header.inc.php');?>
  print "<h1> Uw bestelling is geplaatst. Your order has been placed. Check alstublieft uw Email voor een bevestigings mail. Please check your Email for a confirmation email.</h1>"
+//eventueel een bestelnummer oftewel het id in de bestelling//
 <?php  include('../include/footer.inc.php') ?>
